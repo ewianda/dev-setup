d160741eddc4771498350620bcdb13a7db9b59a3
Elvis Wianda <elvis@benchsci.com>
2022-11-21 09:42:22 -0500

    update vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -100,7 +100,8 @@ update_vim_config() {
 
     if [ -r /dev/tty ]; then
       bash -c '</dev/tty vim "$@"' vim -T dumb '+PlugInstall' '+PlugUpgrade' '+qall!' || true
-      bash -c '</dev/tty vim "$@"' vim -T dumb '+CocInstall coc-tsserver' || true
+      bash -c '</dev/tty vim "$@"' vim -T dumb '+CocInstall coc-tsserver' '+qall' || true
+      bash -c '</dev/tty vim "$@"' vim -T dumb '+CocInstall coc-pyright' '+qall' || true
     elif ! [[ $OSTYPE =~ ^freebsd ]]; then
         if command -v nvim &>/dev/null; then
             nvim --headless -c ':PlugUpgrade' -c ':PlugUpdate' -c ':qall' || true
@@ -120,12 +121,12 @@ install_config() {
     if test -e ~/.gitconfig || test -e $GITCONF; then
         backup ~/.gitconfig
      fi
-    curl -fsSL https://gist.github.com/ambakshi/d111202b21041db55a80/raw -o ${GITCONF}
+    curl -fsSL https://raw.githubusercontent.com/ewianda/dev-setup/Add-bootstrap/gitconfig -o ${GITCONF}
     [ -n "$GNAME" ] && git config --global user.name "$GNAME" || :
     [ -n "$GEMAIL" ] && git config --global user.email "$GEMAIL" || :
     git config --global core.excludesFile '~/.gitignore'
 
-    test -e ~/.tmux.conf || curl -fsSL https://gist.githubusercontent.com/ambakshi/51c994271a216016edef/raw/tmuxconf -o ~/.tmux.conf
+    # test -e ~/.tmux.conf || curl -fsSL https://gist.githubusercontent.com/ambakshi/51c994271a216016edef/raw/tmuxconf -o ~/.tmux.conf
     test -e ~/.inputrc || curl -fsSL https://gist.githubusercontent.com/ambakshi/51c994271a216016edef/raw/inputrc -o ~/.inputrc
     test -e ~/.bash_aliases || curl -fsSL https://gist.githubusercontent.com/ambakshi/51c994271a216016edef/raw/bash_aliases -o ~/.bash_aliases
 
@@ -136,9 +137,9 @@ main() {
 	echo "Installing"
 	#install_node
 	install_fzf
-	#install_vim
-	#install_config
-	update_vim_config
+	install_vim
+	install_config
+	# update_vim_config
 
 
 }
